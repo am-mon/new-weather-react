635eb0437cafb5774eec55c21e855bfe2343ca78
Myat Mon <ayemyatmon.yn@gmail.com>
2025-07-03 13:46:56 +0100

    added ga4

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,34 @@
+import React, { useEffect } from "react";
+import ReactGA from "react-ga4";
+import { BrowserRouter, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 
+const TRACKING_ID = "G-FNNRR964C0";
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.send({
+      hitType: "pageview",
+      page: location.pathname + location.search,
+    });
+  }, [location]);
+
+  return null;
+}
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(TRACKING_ID);
+  }, []);
+
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <AnalyticsTracker />
+        <Home />
+      </BrowserRouter>
     </>
   );
 }
