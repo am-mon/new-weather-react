b8dde92c854c11ffd9b260715db366042f502e44
Myat Mon <ayemyatmon.yn@gmail.com>
2025-07-03 16:22:20 +0100

    updated card

--- src/components/HistoryItemCard.jsx
+++ src/components/HistoryItemCard.jsx
@@ -2,15 +2,15 @@ import { ImBin } from "react-icons/im";
 
 export default function HistoryItemCard({ item, onDelete }) {
   return (
-    <div className="relative text-center shadow bg-white rounded-2xl pt-13 pb-7 px-5">
+    <div className="relative text-center shadow bg-white rounded-2xl pt-8 pb-10 px-5">
       <button
         onClick={() => onDelete(item)}
-        className="absolute z-[2] top-3 right-3 bg-gray-500 hover:bg-gray-700 text-white font-semibold p-2 rounded-xl cursor-pointer"
+        className="absolute z-[2] top-5 right-5 bg-gray-500 hover:bg-gray-700 text-white font-semibold p-2 rounded-xl cursor-pointer"
       >
         <ImBin className="text-lg" />
       </button>
-      <h3 className="font-semibold text-xl relative z-[1]">
-        {item.city}, {item.country}
+      <h3 className="font-semibold text-xl relative z-[1] px-10">
+        {item.city}, <span className="whitespace-nowrap">{item.country}</span>
       </h3>
       <img
         src={`https://openweathermap.org/img/wn/${item.icon}@4x.png`}
